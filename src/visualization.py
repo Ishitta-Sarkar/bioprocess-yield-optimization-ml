270f4079b78dff23405c9e7fcc3966bcbfcd1568
Ishitta Sarkar <ishittas4@gmail.com>
2026-06-21 17:43:16 +0530

    Add bioprocess yield visualization script

---
+++ src/visualization.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+dataset = pd.read_csv("data/bioprocess_dataset.csv")
+
+plt.figure(figsize=(8, 5))
+plt.scatter(
+    dataset["fermentation_time_h"],
+    dataset["product_yield_g_l"]
+)
+
+plt.xlabel("Fermentation Time (h)")
+plt.ylabel("Product Yield (g/L)")
+plt.title("Effect of Fermentation Time on Product Yield")
+plt.tight_layout()
+plt.savefig("figures/fermentation_time_vs_yield.png", dpi=300)
+plt.show()
